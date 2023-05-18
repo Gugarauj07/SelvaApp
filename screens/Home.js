1b2ff4ffdef49acab00d35dc69be4ca107300d76
Gustavo Araújo Lima <92393578+Gugarauj07@users.noreply.github.com>
2023-05-17 23:30:12 -0400

    att

--- screens/Home.js
+++ screens/Home.js
@@ -82,14 +82,20 @@ const Home = ({navigation, route }) => {
 }
 const styles = StyleSheet.create({
   container: {
-    flex:1,
-    backgroundColor: "#fff",
-    alignItems: 'center',
-    justifyContent:'center',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
   },
   map: {
-    flex:1,
-    width: "100%"
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0
   }
 })
 
